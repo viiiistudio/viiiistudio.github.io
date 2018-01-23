48fb1e3ae4bc48a4dcd9d2e7114388f021915b2d
admin <ethino@ethino.com>
2018-01-23 16:19:36 -0500

    logo resize, normal animation now

--- vis.js
+++ vis.js
@@ -1,7 +1,6 @@
 $( document ).ready(function() {
     setTimeout(function(){
       $("#logo").fadeIn().fadeOut().fadeIn("slow", function() {
-        $("#logo").addClass("animated hinge");
       });
     }, 1000);
 });
