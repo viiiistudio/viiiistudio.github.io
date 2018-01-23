ce71b4f71c3cb6bf200226d04d5833d2c5b22299
admin <ethino@ethino.com>
2018-01-23 00:14:47 -0500

    new animation

--- vis.js
+++ vis.js
@@ -1,5 +1,7 @@
 $( document ).ready(function() {
     setTimeout(function(){
-          $("#logo").addClass('animated hinge');
+      $("#logo").fadeIn().fadeOut().fadeIn("slow", function() {
+        $("#logo").addClass("animated hinge");
+      });
     }, 1000);
 });
